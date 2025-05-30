2214aa9bd12a8b6095b5c91e5657a0cabc500db2
Dmitriy Bannov <bannovd@yandex.ru>
2025-05-30 13:43:58 +0300

    fix Dice test

--- tests/test_dice.cpp
+++ tests/test_dice.cpp
@@ -4,6 +4,11 @@
 
 TEST(DiceTest, RollTest) 
 {
-    EXPECT_FALSE(Dice::getInstance().roll() == Dice::getInstance().roll());
+    int r = 0;
+    for (int i = 0; i < 100; ++i)
+    {    
+        r = Dice::getInstance().roll();
+        EXPECT_TRUE(r <= 20 && r >= 1);
+    }    
 }
 
